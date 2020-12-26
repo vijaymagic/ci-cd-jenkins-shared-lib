3814385f900098eb0868b2896f6d682b47e0829c
Nagavijayan Nagarathinam <nagavijayan.nagarathinam@freshworks.com>
2020-12-26 19:15:15 +0530

    Docker build script change

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,5 @@
 def call(String dockerImage, String dockerfile) {
   script {
-    // docker.build "${dockerImage}"
-    docker.build("${dockerImage}", "-f ${dockerfile} ./dockerfiles/")
-    // docker.build("db", "-f docker/db/Dockerfile .")
+    docker.build("${dockerImage}", "-f  ./dockerfiles/${dockerfile} .")
   }
 }
