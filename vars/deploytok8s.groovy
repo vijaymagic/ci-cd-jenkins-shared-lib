0e20b601ed84881509cf354f57580b09846bcd3f
Nagavijayan Nagarathinam <nagavijayan.nagarathinam@freshworks.com>
2020-12-28 12:35:05 +0530

    k8s deployment

--- vars/deploytok8s.groovy
+++ vars/deploytok8s.groovy
@@ -1,6 +1,6 @@
 
 def call(String dockerImage, String app, String service) {
     sh """
-        ansible-playbook ansible-k8s-deployment.yml --extra-vars \"image_id=${dockerImage} app_name=${app} service_name=${service}\"
+        ansible-playbook ansible-k8s-deployment.yaml --extra-vars \"image_id=${dockerImage} app_name=${app} service_name=${service}\"
     """
 }
